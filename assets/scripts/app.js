1e7dd02a26e053b1f4c31e5238261a1148e09c11
Q8Sa <qaiumshah92@gmail.com>
2020-05-11 01:23:07 +0300

    converting data types

---
+++ assets/scripts/app.js
@@ -0,0 +1,47 @@
+const defaultResult = 0;
+let currentResult = defaultResult;  
+
+//get user input number
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+//Generates and writes the calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor.js
+}
+
+function add() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNum;
+    createAndWriteOutput('+', initialResult, enteredNum);
+
+}
+
+function subtract() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNum;
+    createAndWriteOutput('-', initialResult, enteredNum);
+}
+
+function multiply() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNum;
+    createAndWriteOutput('*', initialResult, enteredNum);
+}
+
+function divide() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNum;
+    createAndWriteOutput('/', initialResult, enteredNum);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
